423b941182e3609b54c19aa8b8770933c7281430
ms-online <27732357@qq.com>
2022-01-07 16:30:53 +0800

    lesson-1

--- 01-01-null&undefined/main.js
+++ 01-01-null&undefined/main.js
@@ -1,1 +1,17 @@
+// undefined
+// let x ||let x = 1;
+// console.log(x);
+// console.log(typeof x);
 
+// null
+// let x = null;
+// console.log(x);
+// console.log(Number(x));
+// let y;
+// console.log(Number(y));
+// console.log(x == y);
+// console.log(x === y);
+
+// 不推荐直接为变量赋值为undefined
+let x = undefined;
+console.log(x);
