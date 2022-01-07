8df4ee057c901c71ad03967318633066f146104d
ms-online <27732357@qq.com>
2022-01-07 16:33:54 +0800

    lesson-1

--- 01-01-null&undefined/main.js
+++ 01-01-null&undefined/main.js
@@ -1,4 +1,4 @@
-// undefined
+// undefined:当你没有为js变量分配任何值的时候，为undefined。
 // let x ||let x = 1;
 // console.log(x);
 // console.log(typeof x);
@@ -7,9 +7,15 @@
 // let x = null;
 // console.log(x);
 // console.log(Number(x));
+
 // let y;
 // console.log(Number(y));
+
+// 使用松散比较==，为true，因为它们都表示的是一个无效的值
+// 并且在比较相等性之前，null 没有被转换为其他类型
 // console.log(x == y);
+
+// 严格比较===，为false，两者不属于同一种类型：
 // console.log(x === y);
 
 // 不推荐直接为变量赋值为undefined
