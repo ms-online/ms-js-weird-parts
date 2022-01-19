29a7f7cbcec55a6ba2e06f2779f35b3c16db5108
ms-online <27732357@qq.com>
2022-01-19 12:06:12 +0800

    lesson-19

---
+++ 03-01- extraction-strings/main.js
@@ -0,0 +1,31 @@
+// let str = 'First second third fourth';
+// console.log(str.slice(10));
+// console.log(str.slice(-10, -5));
+// console.log(str.slice());
+
+// let str = 'First second third fourth';
+// console.log(str.substr(10, 4));
+// console.log(str.substr(-10, 5));
+
+// let str = 'First second third fourth';
+// console.log(str.substring(10));
+// console.log(str.substring(-10));
+
+// console.log(str.substring(10, 15));
+
+// let str = 'First second';
+// console.log(str.charAt(3));
+// console.log(str.charAt());
+// console.log(str.charCodeAt(3));
+// console.log(str.charCodeAt());
+
+// console.log(str[3]);
+
+// let str = 'hello world';
+// console.log(str.slice(-5, -2));
+
+// let str = 'This is a test';
+// console.log(str.substring(-5));
+
+let str = 'This is a test';
+console.log(str.slice(3, 3));
