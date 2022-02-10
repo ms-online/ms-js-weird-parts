0fa49bb71b5533072e2cfba49ba0b661bdfad5ba
ms-online <27732357@qq.com>
2022-02-10 16:45:52 +0800

    lesson-26

---
+++ 04-06-destructuring-Object-Literal/main.js
@@ -0,0 +1,33 @@
+// const obj = {
+//   pCode: 1001,
+//   pName: 'Apple',
+// };
+
+// let { pCode, pName } = obj;
+// console.log(pCode, pName);
+
+//数组解构
+// let [a, b, c, d] = arr;
+
+//函数中
+// function test() {
+//   const obj = {
+//     pCode: 1001,
+//     pName: 'Apple',
+//   };
+//   return obj;
+// }
+
+// let { pCode, pName } = test();
+// console.log(pCode, pName);
+
+// function test() {
+//   const obj = {
+//     pCode: 1001,
+//     pName: 'Apple',
+//   };
+//   return obj;
+// }
+
+// let { pCode = 0, pName } = test();
+// console.log(pCode, pName);
