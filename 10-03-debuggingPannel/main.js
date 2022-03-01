5e28b02966811ef0e88df7efb9f2135336679405
ms-online <27732357@qq.com>
2022-03-01 11:50:45 +0800

    lesson-49

--- 10-03-debuggingPannel/main.js
+++ 10-03-debuggingPannel/main.js
@@ -1,15 +1,41 @@
-function sum() {
-  let n1 = document.getElementById('n1').value;
-  let n2 = document.getElementById('n2').value;
-  if (n1 === '' || n2 === '') {
-    showError();
+var inputs = document.querySelectorAll('input');
+var label = document.querySelector('p');
+var button = document.querySelector('button');
+
+//获取输入框的值
+function getNumber1() {
+  return inputs[0].value;
+}
+function getNumber2() {
+  return inputs[1].value;
+}
+//事件监听
+button.addEventListener('click', onClick);
+
+//点击事件
+function onClick() {
+  if (inputsAreEmpty()) {
+    label.textContent = '出错了: 输入框不能为空.';
+    return;
+  }
+  updateLabel();
+}
+//是否为空
+function inputsAreEmpty() {
+  if (getNumber1() === '' || getNumber2() === '') {
+    return true;
   } else {
-    let res = n1 + n2;
-    let result = n1 + '+' + n2 + '=' + res;
-    document.getElementById('output').innerHTML = result;
+    return false;
   }
 }
 
-function showError() {
-  document.getElementById('output').innerHTML = '输入框不能为空!';
+//更新结果
+function updateLabel() {
+  var addend1 = getNumber1();
+  // console.log('addend1', addend1);
+  var addend2 = getNumber2();
+  // console.log('addend2', addend2);
+  var sum = parseInt(addend1) + parseInt(addend2);
+  // console.log('sum', sum);
+  label.textContent = addend1 + ' + ' + addend2 + ' = ' + sum;
 }
